afb83ae1b3274ff74107cd34c81856506c8abc2b
Jeremy Muñoz <jeremyxxo@gmail.com>
2024-05-21 14:04:54 -0400

    Cambios Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <>
-      <div className="cont-ultra-string flex justify-center items-center">
+      <div className="cont-ultra-string flex justify-center  min-h-screen p-4">
         <span className="Ultrareport-string1">ULTRA</span>-
         <span className="Ultrareport-string2 "> REPORT</span>
       </div>
