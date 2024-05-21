fa3b5e561056823103d81894c398ffa090d51c8e
rawcristian710 <108558480+rawcristian@users.noreply.github.com>
2024-05-21 14:00:54 -0400

    footer.edit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <>
-      <div className="cont-ultra-string flex justify-center items-center">
+      <div className="cont-ultra-string flex justify-center items-center bottom-0 sticky">
         <span className="Ultrareport-string1">ULTRA</span>-
         <span className="Ultrareport-string2 "> REPORT</span>
       </div>
