c62854b7daacb80e6c3384faff1783cfd46c56df
Jeremy Muñoz <jeremyxxo@gmail.com>
2024-05-21 12:10:36 -0400

    #

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <>
-      <div className=" inset-inline-end">
+      <div className=" inset-x-0">
         <span className="h-14 w-14 top-0 start-0">Sup.</span>
       </div>
       <div className="img-header  max-w-100 max-h-100 bg-black">
