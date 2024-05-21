7070967d4a3cdad827e0e56f77a59c8333ff7afe
rawcristian710 <108558480+rawcristian@users.noreply.github.com>
2024-05-21 13:57:24 -0400

    footer-center

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
 function Footer() {
   return (
     <>
-      <div className="cont-ultra-string flex ">
-        <span className="Ultrareport-string1"> ULTRA </span>-
-        <span className="Ultrareport-string2"> REPORT</span>
+      <div className="cont-ultra-string flex justify-center items-center">
+        <span className="Ultrareport-string1">ULTRA</span>-
+        <span className="Ultrareport-string2 "> REPORT</span>
       </div>
     </>
   );
