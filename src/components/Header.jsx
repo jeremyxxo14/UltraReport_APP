0ad6dab7e6afa028c169ffd7501abf3d0e9194b0
Jeremy Muñoz <jeremyxxo@gmail.com>
2024-05-21 14:47:06 -0400

    #

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 function Header() {
   return (
     <>
-      <div className="Contendor-Header flex ">
-        <div className="Back-Button mx-5 p-3 rounded-2xl bg-zinc-300 block">
+      <div className="Contendor-Header p-1">
+        <div className="Back-Button mx-5 my-3 y-5 p-3 rounded-2xl bg-zinc-300 inline-block ">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -18,10 +18,10 @@ function Header() {
             />
           </svg>
         </div>
-        <div className="right-0 mx-1 px-10">
-          <span className="absolute right-0 text-zinc-50">Sup.</span>
+        <div className="">
+          <span className="absolute right-0 mx-6 my-8  text-zinc-50">Sup.</span>
         </div>
-        <div className="flex justify-center items-center max-w-100 max-h-100 ">
+        <div className="flex justify-center items-center max-w-100 max-h-100 py-10">
           <img className="w-12" src="authorization.png" alt="" />
         </div>
       </div>
