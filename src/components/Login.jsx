cd036e310c49d5b67afaa69671632e275e45a607
Jeremy Muñoz <jeremyxxo@gmail.com>
2024-05-21 22:48:16 -0400

    #

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,33 +5,26 @@ function Login() {
         <div className="img-Login">
           <img className="w-16" src="authorization.png" alt="" />
         </div>
-        <div class="input flex flex-col w-fit static">
-          <label
-            for="input"
-            className="text-black/25 text-xs font-semibold relative top-2 ml-[7px] px-[3px] bg-[#D9D9D9] w-fit"
-          >
+        <div className="input flex flex-col w-fit static">
+          <label className="text-black/25  text-xs font-semibold relative top-2 ml-[7px] px-[3px] bg-[#D9D9D9] rounded-[5px] w-fit border-2 border-zinc-950">
+            <div className="my-3 left-[0.1px]  absolute w-32 bg-[#D9D9D9] h-2"></div>
             Rut:
           </label>
           <input
             type="text"
             placeholder="......."
-            name="input"
-            className="border-zinc-950 Inputs-Login input px-[10px] py-[11px] text-xs bg-[#D9D9D9] border-2 rounded-[15px] w-[210px] focus:outline-none placeholder:text-black/25"
+            className="border-zinc-950 Inputs-Login input px-[10px] py-[11px] hover:border-zinc-50 text-xs bg-[#D9D9D9] border-2 rounded-[15px] w-[210px] focus:outline-none placeholder:text-black/25"
           />
         </div>
-        <div class="input flex flex-col w-fit static">
-          <label
-            for="input"
-            className="text-black/25  text-xs font-semibold relative top-2 ml-[7px] px-[3px] bg-[#D9D9D9] rounded-[5px] w-fit border-2 border-zinc-950"
-          >
-            <div className="my-3 left-[0.1px]  absolute w-32 bg-[#D9D9D9] h-2"></div>
+        <div className="input flex flex-col w-fit static ">
+          <label className="text-black/25  text-xs font-semibold  after:shadow-red-600 relative top-2 ml-[7px] px-[3px] bg-[#D9D9D9] rounded-[5px] w-fit border-2 border-zinc-950">
+            <div className="my-3 left-[0.1px]  absolute w-32  bg-[#D9D9D9] h-2"></div>
             Contraseña:
           </label>
           <input
             type="password"
             placeholder="......."
-            name="input"
-            className="border-zinc-950 Inputs-Login input px-[10px] py-[11px] text-xs bg-[#D9D9D9] border-2 rounded-[15px] w-[210px] focus:outline-none placeholder:text-black/25"
+            className="border-zinc-950 Inputs-Login input px-[10px] py-[11px] text-xs hover:border-zinc-50 bg-[#D9D9D9] border-2 rounded-[15px] w-[210px] focus:outline-none placeholder:text-black/25"
           />
         </div>
         <div>
