a41f45b9a49a4e986b11f512e043b7ff89dc9f87
rawcristian710 <108558480+rawcristian@users.noreply.github.com>
2024-05-26 18:18:29 -0400

    up

--- src/components/PassAlert.jsx
+++ src/components/PassAlert.jsx
@@ -1,8 +1,14 @@
 export default function PassAlert() {
   return (
-    <div className="ContPassAlert flex flex-col items-center justify-center bg-gray-950 ">
-      <div className="BoxAlertPass text-center">
-        LLENAR CONTRASEÑA CORRECTAMENTE
+    <div className="ContPassAlert flex flex-col items-center justify-center ">
+      <div className="BoxAlertPass flex flex-col items-center justify-center  p-3 border-2 border-solid border-stone-950 w-72  my-10 h-40 rounded-3xl ">
+        <div className="PassString text-center pt-5 ">
+          <p>
+            LLENAR <p1> CONTRASEÑA</p1>
+          </p>
+          <p>CORRECTAMENTE </p>
+          <img src="exclamacion.svg" className="w-14 h-14"></img>
+        </div>
       </div>
     </div>
   );
