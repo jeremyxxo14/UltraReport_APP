afa9dc76dc8538fe4f66cbc8b739da68c66b99cb
Jeremy Muñoz <jeremyxxo@gmail.com>
2024-05-27 11:26:13 -0400

    #

--- src/data/db.js
+++ src/data/db.js
@@ -2,9 +2,8 @@ export const db = [
   {
     id: 1,
     name: "Reporte-J.Alfredo",
-    num:"3547654",
+    num: "3547654",
     description:
       "Morbi ornare augue nisl, vel elementum dui mollis vel. Curabitur non ex id eros fermentum hendrerit.",
-    data:,
   },
 ];
