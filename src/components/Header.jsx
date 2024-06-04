e5ea5a413202126a9fe4504bdb2f086db2c2cc6c
Jeremy Muñoz <jeremyxxo@gmail.com>
2024-06-04 11:22:34 -0400

    # fondo header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <>
-      <div className="Contendor-Header flex  p-5 my-5 items-center justify-between"></div>
+      <div className="Contendor-Header flex bg-[#535353] bg-opacity-70  p-10  mb-20 items-center justify-between"></div>
     </>
   );
 }
