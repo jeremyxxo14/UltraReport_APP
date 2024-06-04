ca016275b72dada50d66ffb3439b9cbad20ea6da
rawcristian710 <108558480+rawcristian@users.noreply.github.com>
2024-06-04 18:27:44 -0400

    creacion home supp

--- src/pages/HomeSup.jsx
+++ src/pages/HomeSup.jsx
@@ -2,18 +2,23 @@ import React from "react";
 import Footer from "../components/Footer";
 import Back from "../components/Back";
 import Header from "../components/Header";
+import ButtonBaseDemo from "../components/SupButtons";
 
 const HomeSup = () => {
   return (
     <div className="Contenedor-APP">
       <Header />
-      <div className="grid grid-cols-3 grid-rows-4 gap-2 h-[100vh]">
+
+      <div className="grid grid-cols-2 grid-rows-3 gap-2 h-[100vh]">
         <div className=" col-span-1 m-3 ">
           <Back />
         </div>
-        <div className="col-end-3 flex flex-col gap-3 justify-self-center "></div>
-        <div className="col-end-3 flex flex-col gap-3 justify-self-center "></div>
-        <div className="col-end-3 flex flex-col gap-3 justify-self-center "></div>
+        <div className="col col-span-1"></div>
+        <div className="col-span-2  flex flex-col  justify-self-center ">
+          <div className="flex flex-row ">
+            <ButtonBaseDemo />
+          </div>
+        </div>
       </div>
       <Footer />
     </div>
